4786610a19abba77be2d179a425065c630e5776e
Jeffrey Tan <i@jeffreytan.org>
2025-05-15 01:39:57 +0800

    add vlm_ros2

---
+++ vlm_ros2/vlm_ros2/image_recognition.py
@@ -0,0 +1,27 @@
+import os
+import base64
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+prompt = "What is in this image?"
+with open("raccoon.jpg", "rb") as image_file:
+    b64_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "input_text", "text": prompt},
+                {"type": "input_image", "image_url": f"data:image/png;base64,{b64_image}"},
+            ],
+        }
+    ],
+)
+
+print(response.output_text)
